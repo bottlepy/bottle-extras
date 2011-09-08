2d7a566b5db8655956de63a6e6e638c5b61fa0ae
Sean M. Collins <sean@coreitpro.com>
2011-09-08 16:25:48 -0400

    Initial version of a Redis plugin

---
+++ redis/setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+import sys
+import os
+from distutils.core import setup
+
+try:
+    from distutils.command.build_py import build_py_2to3 as build_py
+except ImportError:
+    from distutils.command.build_py import build_py
+
+setup(
+    name = 'bottle-redis',
+    version = '0.1',
+    url = 'http://github.com/sc68cal/bottle-extras/',
+    description = 'Redis integration for Bottle.',
+    author = 'Sean M. Collins',
+    author_email = 'sean@coreitpro.com',
+    license = 'MIT',
+    platforms = 'any',
+    py_modules = [
+        'bottle_redis'
+    ],
+    requires = [
+        'bottle (>=0.9)',
+		'redis'
+    ],
+    classifiers = [
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    cmdclass = {'build_py': build_py}
+)
+
