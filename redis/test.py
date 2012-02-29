2496b907390eb47be3dc4e29436d960334a5a121
Sean M. Collins <sean@coreitpro.com>
2012-02-29 16:36:34 -0500

    Add test for optional args

--- redis/test.py
+++ redis/test.py
@@ -25,9 +25,17 @@ class RedisTest(unittest.TestCase):
         self.app({'PATH_INFO':'/', 'REQUEST_METHOD':'GET'}, lambda x, y: None)
 
         @self.app.get('/2')
-        def test(**kw):
+        def test_kw(**kw):
             self.assertFalse('rdb' in kw)
         self.app({'PATH_INFO':'/2', 'REQUEST_METHOD':'GET'}, lambda x, y: None)
+    
+    def test_optional_args(self):
+        self.plugin = self.app.install(redis_plugin.Plugin(database=1))
+
+        @self.app.get('/db/1')
+        def test_db_arg(rdb):
+            self.assertTrue(rdb.connection_pool.connection_kwargs['db'] == 1)
+        self.app({'PATH_INFO':'/db/1', 'REQUEST_METHOD':'GET'}, lambda x,y: None)
         
 if __name__ == '__main__':
     unittest.main()
