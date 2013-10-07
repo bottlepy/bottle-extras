134caf3f4e3a99ab89d68d4ecfe99e30e85be282
Emmanuel Vadot <elbarto@bocal.org>
2013-10-07 19:32:21 +0200

    - Add a password arg

--- redis/bottle_redis.py
+++ redis/bottle_redis.py
@@ -6,10 +6,11 @@ class RedisPlugin(object):
     name = 'redis'
     api =2
 
-    def __init__(self, host='localhost', port=6379, database=0, keyword='rdb'):
+    def __init__(self, host='localhost', port=6379, database=0, password=None, keyword='rdb'):
       self.host = host
       self.port = port
       self.database = database
+      self.password = password
       self.keyword = keyword
       self.redisdb = None
 
@@ -20,7 +21,7 @@ class RedisPlugin(object):
                 raise PluginError("Found another redis plugin with "\
                         "conflicting settings (non-unique keyword).")
         if self.redisdb is None:
-            self.redisdb = redis.ConnectionPool(host=self.host, port=self.port, db=self.database)
+            self.redisdb = redis.ConnectionPool(host=self.host, port=self.port, db=self.database, password=self.password)
 
     def apply(self, callback, route):
         conf = route.config.get('redis') or {}
