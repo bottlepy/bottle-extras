4e189b982faf9cc93a7c1aacfa7103239c0adcb3
Thiago Avelino <thiagoavelinoster@gmail.com>
2014-03-11 08:51:04 -0300

    set new developing version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ except ImportError:
 
 setup(
     name = 'bottle-mongodb',
-    version = '0.2.1',
+    version = '0.2.2',
     url = 'https://github.com/fdouetteau/bottle-mongodb-plugin',
     description = 'MongoDB integration for Bottle',
     author = 'Florian Douetteau',
